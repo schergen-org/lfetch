1ca5b19718b5f4e3062ac0cd1898e837fa707675
wrth1337 <mail@hellowirth.de>
2026-03-07 13:43:31 +0100

    Remove import of the old Basic.lean file.

--- Lfetch.lean
+++ Lfetch.lean
@@ -1,5 +1,4 @@
 -- This module serves as the root of the `Lfetch` library.
 -- Import modules here that should be built as part of the library.
-import Lfetch.Basic
 import Lfetch.Config
 import Lfetch.Core
