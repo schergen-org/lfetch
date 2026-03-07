84b8fbb66b03050450d1525c2f5f5ab619560f21
wrth1337 <mail@hellowirth.de>
2026-03-07 13:42:05 +0100

    Implement new structure for the config-file.

---
+++ Lfetch/Config/Types.lean
@@ -0,0 +1,27 @@
+import Lfetch.Domain.InfoKey
+
+namespace Lfetch
+
+open Lean
+
+structure Colors where
+  primary : String
+  secondary : String
+  accent : String
+  muted : String
+  deriving Repr, Inhabited, FromJson, ToJson
+
+structure InfoGroup where
+  title : Option String := none
+  infos : List InfoKey
+  deriving Repr, Inhabited, FromJson, ToJson
+
+structure Config where
+  colors : Colors
+  groups : List InfoGroup
+  deriving Repr, Inhabited, FromJson, ToJson
+
+def Config.infoKeys (cfg : Config) : List InfoKey :=
+  cfg.groups.flatMap (fun group => group.infos)
+
+end Lfetch
