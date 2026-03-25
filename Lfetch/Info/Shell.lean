523f74237e670f56081fa2153765845ba6c249ed
Jonathan Rech <joni.w.rech@gmail.com>
2026-03-25 11:56:09 +0100

    Get shell name instead of shell path - takes chars until '/' is met (starting at the end)

--- Lfetch/Info/Shell.lean
+++ Lfetch/Info/Shell.lean
@@ -6,7 +6,9 @@ namespace Lfetch.Info.Shell
 /-- Reports the shell path from the current process environment. -/
 def fetch (colors : Lfetch.Colors) : IO Lfetch.Info.InfoLines := do
   match (← IO.getEnv "SHELL") with
-  | some sh => pure [Lfetch.Info.textDoc sh]
+  | some sh =>
+    let name := sh.takeEndWhile (fun c => c != '/') |> toString
+    pure [Lfetch.Info.textDoc name]
   | none    => pure [Lfetch.Info.unknownDoc colors]
 
 end Lfetch.Info.Shell
