8463c2062b0397d69551ea664930fde5dffbc1c9
Jonathan Rech <joni.w.rech@gmail.com>
2026-03-25 16:43:37 +0100

    Add version detection

--- Lfetch/Info/Shell.lean
+++ Lfetch/Info/Shell.lean
@@ -3,12 +3,66 @@ import Lfetch.Info.Common
 
 namespace Lfetch.Info.Shell
 
-/-- Reports the shell path from the current process environment. -/
+-- From https://github.com/dylanaraps/neofetch/blob/master/neofetch
+/-- Run shell commands -/
+private def runShell (shell cmd : String) : IO String := do
+  let out ← IO.Process.output {
+    cmd := shell
+    args := #["-c", cmd]
+  }
+  return out.stdout.trimAscii.toString
+
+/-- Removes the first occurrence of a substring -/
+private def removeFirst (s sub : String) : String :=
+  match s.splitOn sub with
+  | []      => s
+  | x :: xs => x ++ String.intercalate sub xs
+
+private def getShellVersion (shellName : String) : IO String := do
+  match shellName with
+  | "bash" => do
+      let v ← runShell "bash" "printf %s \"$BASH_VERSION\""
+      IO.println s!"Bash version output: {v}"
+      pure (v.splitOn "(" |>.headD v)
+
+  | "sh" | "ash" | "dash" | "es" =>
+      pure ""
+
+  | "osh" =>
+      let v ← runShell "osh" "printf %s \"$OIL_VERSION\""
+      pure v
+
+  | "tcsh" => do
+      runShell "tcsh" "printf %s $tcsh"
+
+  | "yash" => do
+      let v ← runShell "yash" "--version"
+      let v := removeFirst v s!" {shellName}"
+      let v := removeFirst v " Yet another shell"
+      let v := removeFirst v "Copyright"
+      pure v
+
+  | "nu" => do
+      let v ← runShell "nu" "version | get version"
+      pure (removeFirst v s!" {shellName}")
+
+  | name =>
+    if name.endsWith "ksh" then do
+      let v ← runShell name "printf %s \"$KSH_VERSION\""
+      let v := removeFirst v " KSH"
+      let v := removeFirst v "version"
+      pure v
+    else do
+      let v ← runShell name "--version"
+      pure (removeFirst v s!" {shellName}")
+
+/-- Reports the shell path and version from the current process environment. -/
 def fetch (colors : Lfetch.Colors) : IO Lfetch.Info.InfoLines := do
   match (← IO.getEnv "SHELL") with
   | some sh =>
     let name := sh.takeEndWhile (fun c => c != '/') |> toString
-    pure [Lfetch.Info.textDoc name]
+    let version ← getShellVersion name
+    pure [Lfetch.Info.textDoc (name ++ " " ++ version)]
   | none    => pure [Lfetch.Info.unknownDoc colors]
 
 end Lfetch.Info.Shell
